827cae3a919770104f08d3312f49318a11293e2d
JOEX <oyinjoe23@gmail.com>
2023-10-25 14:09:29 +0100

    feat: document's schema and api Endpoint

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,28 @@
 "use client";
 
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
-import Image from "next/image";
+import { useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note",
+    });
+  };
+
   return (
     <div className=" h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -26,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className=" text-lg font-medium">
         welcome to {user?.firstName}&apos;s Jation
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className=" h-4 w-4 mr-2" />
         Create a note
       </Button>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -92,6 +99,17 @@ export const Navigation = () => {
     }
   };
 
+
+  const handleCreate = () => {
+    const promise = create({title: "Untitled"})
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note",
+    })
+  }
+
   return (
     <>
       <aside
@@ -114,9 +132,12 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className=" mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id}>{document.title}</p>
+          ))}
         </div>
         <div
           onMouseDown={handleMouseDown}
